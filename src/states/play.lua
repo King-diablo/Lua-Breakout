6791e7079e92f90baab5ad18098b552974cd27ca
Chikeluba Obiora-Eze <chikelubaobioraeze@gmail.com>
2026-05-01 13:46:40 +0100

    - proper collision - bug fixes

--- src/states/play.lua
+++ src/states/play.lua
@@ -35,12 +35,51 @@ function Play:update(dt)
 
     if self.ball:collides(self.paddle) then
         -- reverse Y velocity if collision detected between paddle and ball
+        self.ball.y = self.paddle.y - 8
         self.ball.dy = -self.ball.dy
+        local isPaddleMovingLeft = self.paddle.dx < 0
+        local isPaddleMovingRight = self.paddle.dx > 0
+        local paddleCenter = self.paddle.x + self.paddle.width / 2
+
+        local startingBounceDX = 50
+        local bouncingMultiplier = 8
+
+        if self.ball.x < paddleCenter and isPaddleMovingLeft then
+            local ballOffset = paddleCenter - self.ball.x
+            self.ball.dx = -startingBounceDX - bouncingMultiplier * ballOffset
+        elseif self.ball.x > paddleCenter and isPaddleMovingRight then
+            local ballOffset = self.ball.x - paddleCenter
+            self.ball.dx = startingBounceDX + bouncingMultiplier * ballOffset
+        end
         gSound['paddle-hit']:play()
     end
+
     for k, brick in pairs(self.bricks) do
         if brick.inPlay and self.ball:collides(brick) then
             brick:hit()
+            local BALL_RADIUS = 4
+            local BRICK_W, BRICK_H = brick.width, brick.height
+
+            local cxB, cyB = brick.x + BRICK_W / 2, brick.y + BRICK_H / 2
+            local cxb, cyb = self.ball.x + BALL_RADIUS, self.ball.y + BALL_RADIUS
+
+            local ox = cxB - cxb
+            local oy = cyB - cyb
+
+            local px = BRICK_W / 2 + BALL_RADIUS - math.abs(ox)
+            local py = BRICK_H / 2 + BALL_RADIUS - math.abs(oy)
+
+            if px < py then
+                self.ball.dx = -self.ball.dx
+                self.ball.x = self.ball.x + (ox > 0 and -px or px)
+            else
+                self.ball.dy = -self.ball.dy
+                self.ball.y = self.ball.y + (oy > 0 and -py or py)
+            end
+
+            self.ball.dy = self.ball.dy * 1.02
+
+            break
         end
     end
 
@@ -48,6 +87,7 @@ function Play:update(dt)
         love.event.quit()
     end
 end
+
 
 function Play:render()
     for k, brick in pairs(self.bricks) do
