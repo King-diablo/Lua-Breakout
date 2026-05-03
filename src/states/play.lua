8b31a6ec04b5ccb9c9e9e342c01733b075dde177
Chikeluba Obiora-Eze <chikelubaobioraeze@gmail.com>
2026-05-03 11:40:53 +0100

    - add particles

--- src/states/play.lua
+++ src/states/play.lua
@@ -95,6 +95,10 @@ function Play:update(dt)
             })
         end
     end
+    -- for rendering particle systems
+    for k, brick in pairs(self.bricks) do
+        brick:update(dt)
+    end
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
@@ -104,6 +108,9 @@ end
 function Play:render()
     for k, brick in pairs(self.bricks) do
         brick:render()
+    end
+    for k, brick in pairs(self.bricks) do
+        brick:renderParticles()
     end
     self.paddle:render()
     self.ball:render()
